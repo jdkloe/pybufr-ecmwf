7cd7e40f16860826ff2b73ab4603d2ba03fae905
Jos de Kloe <kloedej@knmi.nl>
2009-11-20 15:16:57 +0100

    added a little clean script

---
+++ clean.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os,glob
+
+dir_to_delete   = ["ecmwf_bufr_lib/bufr_000380","f2py_build"]
+files_to_delete = glob.glob("*~")
+files_to_delete.extend(["libbufr.a"])
+
+for d in dir_to_delete:
+    if (os.path.exists(d)):
+        print "deleting dir: ",d
+        os.system(r"\rm -rf "+d)
+    # this only works if the dirs are empty!
+    #os.removedirs(d)
+
+for f in files_to_delete:
+    if (os.path.exists(f)):
+        print "deleting file: ",f
+        os.remove(f)
+        #os.system(r"\rm -f "+f)
+    if (os.path.islink(f)):
+        print "deleting symlink: ",f
+        os.remove(f)
+
+    
+#os.remove(
+    
