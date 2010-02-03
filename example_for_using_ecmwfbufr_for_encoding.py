0ffc3643f56dd9189d4015a4777e7142cb29b6aa
Jos de Kloe <josdekloe@gmail.com>
2010-02-03 22:17:44 +0100

    added some replication and delayed replication examples

--- example_for_using_ecmwfbufr_for_encoding.py
+++ example_for_using_ecmwfbufr_for_encoding.py
@@ -138,8 +138,7 @@ bufr_compression_flag     =   0 #  64=compression/0=no compression
 (year,month,day,hour,minute,second,
  weekday,julianday,isdaylightsavingstime) = time.localtime()
 
-#num_subsets = 4
-num_subsets = 8
+num_subsets = 4
 
 # fill section 0
 ksec0[1-1]= 0
@@ -178,46 +177,65 @@ ksec3[3-1]= num_subsets                # no of data subsets
 ksec3[4-1]= bufr_compression_flag      # compression flag
 
 # define a descriptor list
-ktdlen = 6 # length of unexpanded descriptor list
+ktdlen = 9 # length of unexpanded descriptor list
 ktdlst = np.zeros(ktdlen,dtype=np.int)
 
-# add descriptor 1
+# define descriptor 1
 dd_d_date_YYYYMMDD = 301011 # date
 # this defines the sequence:
 # 004001 ! year
 # 004002 ! month
 # 004003 ! day
         
-# add descriptor 2
+# define descriptor 2
 dd_d_time_HHMM = 301012 # time 
 # this defines the sequence:
 # 004004 ! hour 
 # 004005 ! minute 
 
-# add descriptor 3
+# define descriptor 3
 dd_pressure = int('007004',10) # pressure [pa]  
 
 # WARNING: filling the descriptor variable with 007004 will fail
 # because python will interpret this as an octal value, and thus
 # automatically convert 007004 to the decimal value 3588
 
-# add descriptor 4
+# define descriptor 4
 dd_temperature = int('012001',10) # [dry-bulb] temperature [K]  
 
-# add descriptor 5
+# define descriptor 5
 dd_latitude_high_accuracy = int('005001',10)
 # latitude (high accuracy) [degree] 
 
-# add descriptor 6
+# define descriptor 6
 dd_longitude_high_accuracy = int('006001',10)
 # longitude (high accuracy) [degree] 
 
+# define the delayed replication code
+Delayed_Descr_Repl_Factor = int('031001',10)
+
+def get_replication_code(num_descriptors,num_repeats):
+   repl_factor = 100000 + num_descriptors*1000 + num_repeats
+   # for example replicating 2 descriptors 25 times will be encoded as: 102025
+   # for delayed replication, set num_repeats to 0
+   # then add the Delayed_Descr_Repl_Factor after this code
+   return repl_factor
+
 ktdlst[0] = dd_d_date_YYYYMMDD
 ktdlst[1] = dd_d_time_HHMM
-ktdlst[2] = dd_pressure
-ktdlst[3] = dd_temperature
-ktdlst[4] = dd_latitude_high_accuracy
-ktdlst[5] = dd_longitude_high_accuracy
+
+# delay replication for the next 2 descriptors
+ktdlst[2] = get_replication_code(2,0)
+ktdlst[3] = Delayed_Descr_Repl_Factor # = 031001
+
+ktdlst[4] = dd_pressure
+ktdlst[5] = dd_temperature
+
+# replicate the next 2 descriptors 3 times
+ktdlst[6] = get_replication_code(2,3)
+
+ktdlst[7] = dd_latitude_high_accuracy
+ktdlst[8] = dd_longitude_high_accuracy
 
 # call BUXDES
 # buxdes: expand the descriptor list
@@ -230,17 +248,28 @@ if (iprint == 1):
     print "------------------------"
     print " printing BUFR template "
     print "------------------------"
-    
-kdata = np.zeros(1,dtype=np.int) # list of replication factors 
+
+# define and fill the list of replication factors
+num_del_repl_factors = 1
+kdata = np.zeros(num_subsets*num_del_repl_factors,dtype=np.int)
+for i in range(num_subsets):
+    # Warning: just set the whole array to the maximum you wish to have.
+    # Letting this number vary seems not to work with the current
+    # ECMWF library. It will allways just look at the first element
+    # in the kdata array. (or do I misunderstand the BUFR format here?)
+    kdata[i] = 2 # i+1
+print "delayed replication factors: ",kdata
+
 ecmwfbufr.buxdes(iprint,ksec1,ktdlst,kdata,
                  ktdexl,ktdexp,cnames,cunits,kerr)
 print "ktdlst = ",ktdlst
-print "ktdexp = ",ktdexp
+selection = np.where(ktdexp>0)
+print "ktdexp = ",ktdexp[selection]
 print "ktdexl = ",ktdexl # this one seems not to be filled ...?
 if (kerr != 0):
     print "kerr = ",kerr
     sys.exit(1)
-    
+
 #print "cnames = ",cnames
 #print "cunits = ",cunits
 
@@ -262,10 +291,16 @@ for subset in range(num_subsets):
     i=i+1; values[i] =   31 # day
     i=i+1; values[i] =   23 # hour
     i=i+1; values[i] =   59    -        subset # minute
-    i=i+1; values[i] = 1013.e2 - 100.e2*subset # pressure [pa]
-    i=i+1; values[i] = 273.15  -    10.*subset # temperature [K]
-    i=i+1; values[i] = 51.82   +   0.05*subset # latitude
-    i=i+1; values[i] =  5.25   +    0.1*subset # longitude
+    i=i+1; values[i] = 2 # delayed replication factor
+    # this delayed replication factor determines the actual number
+    # of values to be stored for this particular subset
+    # even if it is less then the number given in kdata above !
+    for repl in range(2):
+	i=i+1; values[i] = 1013.e2 - 100.e2*subset+i # pressure [pa]
+	i=i+1; values[i] = 273.15  -    10.*subset+i # temperature [K]
+    for repl in range(3):
+	i=i+1; values[i] = 51.82   +   0.05*subset+i # latitude
+	i=i+1; values[i] =  5.25   +    0.1*subset+i # longitude
         
 # call BUFREN
 #   bufren: encode a bufr message
