ced2f1c557c0e3d00e516f15f1844d1319b3200f
Jos de Kloe <kloedej@knmi.nl>
2010-03-19 12:33:26 +0100

    implemented all additional non-standard setup.py commandline options that I need to build the fortran-to-c interface

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@
 # set the DISTUTILS_DEBUG environment variable to TRUE
 # (or any other non-empty string)
 
+#  #[ imported modules
 import os
 import sys
 from distutils.core import setup, Extension
-# import build_ext using a different name,
-# to allow subclassing it
+# import build and build_ext using a different name,
+# to allow subclassing them
+from distutils.command.build import build as _build
 from distutils.command.build_ext import build_ext as _build_ext
 
 # patch distutils if it can't cope with the "classifiers" or
@@ -18,8 +20,61 @@ if version < '2.2.3':
     from distutils.dist import DistributionMetadata
     DistributionMetadata.classifiers = None
     DistributionMetadata.download_url = None
+#  #]
 
+# modify the build class to allow some custom commandline
+# and setup.cfg options to the build stage
+class build(_build):
+    #  #[
+    """Adapted Python binary builder."""
+    user_options = _build.user_options
+    user_options.append(("preferred-fortran-compiler",None,
+                         "name of preferred fortran compiler to be used"))
+    user_options.append(("preferred-c-compiler",None,
+                         "name of preferred c compiler to be used"))
+    user_options.append(("fortran-compiler",None,
+                         "name and full path of fortran compiler to be used"))
+    user_options.append(("fortran-ld-library-path",None,
+                         "path in which shared objects can be found that "+\
+                         "are needed by the choosen fortran compiler"))
+    user_options.append(("fortran-flags",None,
+                         "flags to be passed to the fortran compiler"))
+    user_options.append(("c-compiler",None,
+                         "name and full path of c compiler to be used"))
+    user_options.append(("c-ld-library-path",None,
+                         "path in which shared objects can be found that "+\
+                         "are needed by the choosen c compiler"))
+    user_options.append(("c-flags",None,
+                         "flags to be passed to the c compiler"))
+
+    def initialize_options(self):
+        # initialise the additional options 
+        self.preferred_fortran_compiler = None
+        self.preferred_c_compiler = None
+        self.fortran_compiler = None
+        self.fortran_ld_library_path = None
+        self.fortran_flags = None
+        self.c_compiler = None
+        self.c_ld_library_path = None
+        self.c_flags = None
+        _build.initialize_options(self)
+
+    def run(self):
+        # some test prints:
+        #print "build: self.user_options = ",self.user_options
+        #print "build: self.preferred_fortran_compiler = ",\
+        #      self.preferred_fortran_compiler
+        #print "build: self.preferred_c_compiler = ",\
+        #      self.preferred_c_compiler
+
+        # call the run command of the default build
+        _build.run(self)
+    #  #]
+
+# modify the build_ext class to allow some custom commandline
+# and setup.cfg options to the build_ext stage
 class build_ext(_build_ext):
+    #  #[
     """Specialized Python extension builder."""
     # implement whatever needs to be different...
     # see the original build_ext.py in:
@@ -32,11 +87,31 @@ class build_ext(_build_ext):
                          "name of fortran compiler to be used"))
     user_options.append(("preferred-c-compiler",None,
                          "name of c compiler to be used"))
+    user_options.append(("fortran-compiler",None,
+                         "name and full path of fortran compiler to be used"))
+    user_options.append(("fortran-ld-library-path",None,
+                         "path in which shared objects can be found that "+\
+                         "are needed by the choosen fortran compiler"))
+    user_options.append(("fortran-flags",None,
+                         "flags to be passed to the fortran compiler"))
+    user_options.append(("c-compiler",None,
+                         "name and full path of c compiler to be used"))
+    user_options.append(("c-ld-library-path",None,
+                         "path in which shared objects can be found that "+\
+                         "are needed by the choosen c compiler"))
+    user_options.append(("c-flags",None,
+                         "flags to be passed to the c compiler"))
 
     def initialize_options(self):
         # initialise the additional options 
         self.preferred_fortran_compiler = None
         self.preferred_c_compiler = None
+        self.fortran_compiler = None
+        self.fortran_ld_library_path = None
+        self.fortran_flags = None
+        self.c_compiler = None
+        self.c_ld_library_path = None
+        self.c_flags = None
         _build_ext.initialize_options(self)
 
     def finalize_options (self):
@@ -44,26 +119,54 @@ class build_ext(_build_ext):
         # to the build_ext class, so I'll have to modify
         # the build class as well to allow new options
         self.set_undefined_options('build',
-                                   ('preferred_fortran_compiler', 'preferred_fortran_compiler'),
-                                   ('preferred_c_compiler', 'preferred_c_compiler'),
+                                   ('preferred_fortran_compiler',
+                                    'preferred_fortran_compiler'),
+                                   ('preferred_c_compiler',
+                                    'preferred_c_compiler'),
+                                   ('fortran_compiler',
+                                    'fortran_compiler'),
+                                   ('fortran_ld_library_path',
+                                    'fortran_ld_library_path'),
+                                   ('fortran_flags',
+                                    'fortran_flags'),
+                                   ('c_compiler','c_compiler'),
+                                   ('c_ld_library_path',
+                                    'c_ld_library_path'),
+                                   ('c_flags','c_flags')
                                    )
         _build_ext.finalize_options(self)
         
     def run(self):
-        # call the run command of the default build_py
+        # call the run command of the default build_ext
         # (actually I dont know how to build fortran this way,
         #  so I'll use my own build script here in stead
-        #_build_ext.run(self)
+        #  _build_ext.run(self)  )
+
+        print "python executable: ",sys.executable
 
         # for now, this import triggers the build of the ecmwfbufr.so
         # shared-object file needed by this module
         import pybufr_ecmwf
 
-        print "self.user_options = ",self.user_options
-        print "self.preferred_fortran_compiler = ",\
+        # test prints
+        #print "build_ext: self.user_options = ",self.user_options
+        print "build_ext: self.preferred_fortran_compiler = ",\
               self.preferred_fortran_compiler
-        print "self.preferred_c_compiler = ",\
+        print "build_ext: self.preferred_c_compiler = ",\
               self.preferred_c_compiler
+        print "build_ext: self.fortran_compiler = ",\
+              self.fortran_compiler
+        print "build_ext: self.fortran_ld_library_path = ",\
+              self.fortran_ld_library_path
+        print "build_ext: self.fortran_flags = ",\
+              self.fortran_flags
+        print "build_ext: self.c_compiler = ",\
+              self.c_compiler
+        print "build_ext: self.c_ld_library_path = ",\
+              self.c_ld_library_path
+        print "build_ext: self.c_flags = ",\
+              self.c_flags
+    #  #]
         
 descr="a python interface around the ECMWF-BUFR library."
 long_descr="""a python interface around the Fortran90 ECMWF-BUFR library
@@ -82,12 +185,13 @@ cl = ["Development Status :: Alpha",
       "Environment :: Console"
       "Intended Audience :: Developers"
       "Intended Audience :: System Administrators"
-      "Operating System :: POSIX",
-      ""]
+      "Operating System :: POSIX"
+      ]
 
 Ext=Extension('pybufr_ecmwf.ecmwfbufr',[])
 
-setup(cmdclass={'build_ext': build_ext},
+setup(cmdclass={'build': build,
+                'build_ext': build_ext},
       name='pybufr-ecmwf',
       version='0.1',
       description='Python wrapper around the ECMWF BUFR library',
@@ -103,6 +207,8 @@ setup(cmdclass={'build_ext': build_ext},
       ext_modules=[Ext],
       requires=["numpy","numpy.f2py","subprocess"],
       )
+
+#  #[ usage examples for setup.py:
 
 # (see: http://docs.python.org/distutils/introduction.html)
 # possible uses of this setup script:
@@ -127,8 +233,12 @@ setup(cmdclass={'build_ext': build_ext},
 # creation of a binary distribution tgz file: [works]
 # ==>python setup.py bdist
 
-# creation of an rpm file: [works]
+# creation of an rpm file: [works!]
 # ==>python setup.py bdist_rpm
+# NOTE: for a locally installed python version the command should be:
+# python2.6 ./setup.py bdist_rpm --python python2.6
+# but this fails due to some path problem on my KNMI machine:
+#  File not found: /nobackup/users/kloedej/temp_mercurial_repos/pybufr_ecmwf_copy/build/bdist.linux-i686/rpm/BUILD/pybufr-ecmwf-root/nobackup/users/kloedej/software_installed/scipy_numpy/lib/python2.6/site-packages/pybufr_ecmwf/ecmwfbufr.so
 
 # build by an end user
 # ==>python setup.py build
@@ -136,3 +246,4 @@ setup(cmdclass={'build_ext': build_ext},
 # installation by an end-user
 # ==>python setup.py install
 
+#  #]
