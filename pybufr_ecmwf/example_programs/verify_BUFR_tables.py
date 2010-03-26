70f3874f99eb68831f89cfd16f29d2545bccabd1
Jos de Kloe <josdekloe@gmail.com>
2010-03-26 23:30:27 +0100

    added some explanations

--- pybufr_ecmwf/example_programs/verify_BUFR_tables.py
+++ pybufr_ecmwf/example_programs/verify_BUFR_tables.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# this small example program loads the BUFR B- and D-tables
+# and reports any inconsistencies it finds in the table definitions
+
 from pybufr_ecmwf import bufr
 
 table_code = "0000000000098000000"
