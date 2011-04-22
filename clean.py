1ba55468985f0c07819b4b2c24169476e966c67b
Jos de Kloe <josdekloe@gmail.com>
2011-04-22 16:25:51 +0200

    small fix in the clean.py tool

--- clean.py
+++ clean.py
@@ -10,27 +10,28 @@ from __future__ import print_function
 import os, glob #, sys
 
 # delete these dirs
-DIRS_GLOB_PATTERNS = ["pybufr_ecmwf/ecmwf_bufr_lib/bufr_000*",
-                      "ecmwf_bufr_lib/bufr_000*"]
-DIRS_TO_DELETE = ["pybufr_ecmwf/example_programs/tmp_BUFR_TABLES",
-                  "pybufr_ecmwf/tmp_BUFR_TABLES", "tmp_BUFR_TABLES",
-                  "ecmwf_bufrtables", "pybufr_ecmwf/ecmwf_bufrtables",
-                  "pybufr_ecmwf/f2py_build", "build", "dist",
-                  "tmp_2to3_converted_sources"]
+DIRS_GLOB_PATTERNS = ['pybufr_ecmwf/ecmwf_bufr_lib/bufr_000*',
+                      'ecmwf_bufr_lib/bufr_000*']
+DIRS_TO_DELETE = ['pybufr_ecmwf/example_programs/tmp_BUFR_TABLES',
+                  'pybufr_ecmwf/tmp_BUFR_TABLES', 'tmp_BUFR_TABLES',
+                  'ecmwf_bufrtables', 'pybufr_ecmwf/ecmwf_bufrtables',
+                  'pybufr_ecmwf/f2py_build', 'build', 'dist',
+                  'tmp_2to3_converted_sources',
+                  'example_programs/tmp_BUFR_TABLES']
 
 # delete these files
-FILE_GLOB_PATTERNS = ["*~", "*/*~", "*/*/*~", "*.pyc", "*/*.pyc",
-                      "*/Testfile3Msgs.BUFR", "*/*/Testfile3Msgs.BUFR",
-                      "pybufr_ecmwf/expected_test_outputs/*.actual_std*",
-                      "pylint_*.txt"]
-FILES_TO_DELETE = ["pybufr_ecmwf/libbufr.a", "libbufr.a", "MANIFEST",
-                   "pybufr_ecmwf/ecmwfbufr.so", "ecmwfbufr.so",
-                   "ecmwfbufr.so", "pybufr_ecmwf/ecmwf_bufr_lib/config_file",
-                   "ecmwf_bufr_lib/config_file", "pybufr_ecmwf/ecmwfbufr.so",
-                   "pybufr_ecmwf/ecmwfbufr_parameters.py",
-                   "example_programs/testdata/Testoutputfile1.BUFR",
-                   "example_programs/testdata/Testoutputfile2.BUFR",
-                   "example_programs/testdata/Testoutputfile3.BUFR"]
+FILE_GLOB_PATTERNS = ['*~', '*/*~', '*/*/*~', '*.pyc', '*/*.pyc',
+                      '*/Testfile3Msgs.BUFR', '*/*/Testfile3Msgs.BUFR',
+                      'pybufr_ecmwf/expected_test_outputs/*.actual_std*',
+                      'pylint_*.txt']
+FILES_TO_DELETE = ['pybufr_ecmwf/libbufr.a', 'libbufr.a', 'MANIFEST',
+                   'pybufr_ecmwf/ecmwfbufr.so', 'ecmwfbufr.so',
+                   'ecmwfbufr.so', 'pybufr_ecmwf/ecmwf_bufr_lib/config_file',
+                   'ecmwf_bufr_lib/config_file', 'pybufr_ecmwf/ecmwfbufr.so',
+                   'pybufr_ecmwf/ecmwfbufr_parameters.py',
+                   'example_programs/testdata/Testoutputfile1.BUFR',
+                   'example_programs/testdata/Testoutputfile2.BUFR',
+                   'example_programs/testdata/Testoutputfile3.BUFR']
 
 #pylint: disable-msg=C0103
 dirs_to_delete = []
@@ -49,17 +50,17 @@ for pattern in FILE_GLOB_PATTERNS:
 for d in dirs_to_delete:
     if (os.path.exists(d)):
         if (os.path.isdir(d)):
-            print("deleting dir: ", d)
-            os.system(r"\rm -rf "+d)
+            print('deleting dir: ', d)
+            os.system(r'\rm -rf '+d)
     # this only works if the dirs are empty!
     #os.removedirs(d)
 
 for f in files_to_delete:
     if (os.path.exists(f)):
-        print("deleting file: ", f)
+        print('deleting file: ', f)
         os.remove(f)
     if (os.path.islink(f)):
-        print("deleting symlink: ", f)
+        print('deleting symlink: ', f)
         os.remove(f)
 
-print("done")
+print('done')
