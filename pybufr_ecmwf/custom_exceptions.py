d77b3d768c032dc1ea244abe409beeb2af5d8058
Jos de Kloe <josdekloe@gmail.com>
2014-11-20 20:40:00 +0100

    add custom exceptions

---
+++ pybufr_ecmwf/custom_exceptions.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+"""
+this file defines custom exceptions to be used by this module.
+All these exceptions are derived from the PybufrEcmwfBaseError class
+to enable users to easily catch them all.
+"""
+
+# Written by: J. de Kloe, KNMI (www.knmi.nl)
+#
+# License: GPL v2.
+#
+
+# see: http://docs.python.org/library/exceptions.html
+# for a list of already available exceptions.
+# are:     IOError, EOFError
+
+class PybufrEcmwfBaseError(Exception):
+    """ a generic base exception for this module """
+    pass
+
+class EcmwfBufrLibError(PybufrEcmwfBaseError):
+    """ an exception to indicate that one of the subroutines or functions
+    in the ECMWF bufr library returned with an error """
+    pass
+
+class EcmwfBufrTableError(PybufrEcmwfBaseError):
+    """ an exception to indicate that no set of suitable BUFR tables
+    needed for bufr decoding/encoding can be found """
+    pass
+
+class IncorrectUsageError(PybufrEcmwfBaseError):
+    """ an exception to indicate that the user tried to use
+    a method incorrectly. """
+    pass
+# todo:
+# add a check here to ensure a (hopefully) helpfull error text
+# is provided explaining to the user what to do.
+
+class NoMsgLoadedError(PybufrEcmwfBaseError):
+    """ an exception to indicate that no message has yet been
+    loaded from the currently open BUFR file. """
+    pass
+
+class CannotExpandFlagsError(PybufrEcmwfBaseError):
+    """ an exception to indicate that the user tried to expand
+    flags,  but used this in a wrong way. """
+    pass
+
+class ProgrammingError(PybufrEcmwfBaseError):
+    """ an exception to indicate that a programming error seems
+    present in the code (this should be reported to the author) """
+    pass
+
+# build exceptions, used by the build_interface.py script
+class BuildException(PybufrEcmwfBaseError):
+    """ a generic base exception for this build script """
+    pass
+
+class NetworkError(BuildException):
+    """ an exception to indicate that a network problem occurred """
+    pass
+
+class LibraryBuildError(BuildException):
+    """ an exception to indicate that building the ECMWF BUFR
+    library has failed """
+    pass
+
+class InterfaceBuildError(BuildException):
+    """ an exception to indicate that building the fortran-to-python
+    interface has failed """
+    pass
+
